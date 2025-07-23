7bf99194d55cf36a8554a81066a32affca64c5dd
kimhyeji <gpwl5589@gamil.com>
2025-07-23 16:32:13 +0900

    /home/returnCar, Lombok

--- src/main/java/com/std/sbb/HomeController.java
+++ src/main/java/com/std/sbb/HomeController.java
@@ -1,9 +1,16 @@
 package com.std.sbb;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // @Controller의 의미
 // 개발자가 스프링부트에게 말한다.
@@ -94,4 +101,38 @@ public class HomeController {
 
         return -1;
     }
+
+    @GetMapping("/home/returnCar")
+    @ResponseBody
+    public Car showReturnCar() {
+        Car car = new Car(1, 100, "k5", new ArrayList(){{
+            add(1);
+            add(2);
+            add(3);
+        }});
+
+        car.setId(2);
+        car.setSpeed(200);
+        car.setName("랭글러");
+        car.setIds(new ArrayList(){{
+            add(4);
+            add(5);
+            add(6);
+        }});
+
+        return car;
+    }
+}
+
+// 전체 속성을 포함하는 생성자 함수 생성
+// 게터, 세터 생성
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+class Car {
+    private int id;
+    private int speed;
+    private String name;
+    private List<Integer> ids;
 }
